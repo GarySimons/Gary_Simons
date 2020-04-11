e9fe753fcf226a2c8f4ffb1b5b35ab14ef484b57
GarySimons <garycsimons@gmail.com>
2020-04-11 15:56:55 +0000

    Created an Environmental Variable, and trying to connect to MongoDB

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from os import path
+if path.exists("env.py"):
+  import env 
 
 app = Flask(__name__)
 
+MONGODB_URI = os.environ.get("MONGO_URI")
+DBS_NAME = "skills_manager"
+COLLECTION_NAME = "skills_manager"
+
+mongo = PyMongo(app)
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_skill')
+def get_skill():
+    return render_template("skill.html", skill=mongo.db.skill.find())
 
 
 if __name__ == '__main__':
